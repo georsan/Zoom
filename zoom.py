835384c3ff383007d8d9b87b7496184ec7964215
georsan <jorgesandoval5292gmail.com>
2021-06-30 13:24:33 -0500

    base

--- zoom.py
+++ zoom.py
@@ -3,7 +3,31 @@ import datetime
 import time
 import webbrowser
 def iniciar():
-    webbrowser.open('https://zoom.us/j/91225335028',new=1,autoraise=True)    
+    webbrowser.open('https://renata.zoom.us/j/97048738440#success',new=1,autoraise=True)   
+    clave="123456"    
+    while True:
+        reunion=pyautogui.locateOnScreen('./img/reunion.png')
+        if reunion is None:            
+            print('No click')
+        else:
+            pyautogui.click(reunion)
+            print('Click')
+            unir(clave)
+            break
+
+def unir(clave):
+    while True:
+        join=pyautogui.locateOnScreen('./img/password.png')
+        print(join)
+        if join is None:            
+            print('No clave')
+        else:
+            pyautogui.typewrite(clave)
+            pyautogui.click(pyautogui.locateOnScreen('./img/join.png'))
+            print('Clave')
+            
+            break
+
 
 
 iniciar()
